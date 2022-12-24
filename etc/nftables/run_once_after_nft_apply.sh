28ecb1fd72a025d9213191b8c4cdd42576d47f70
Alex Lov <alex@alexlov.com>
2022-12-24 14:23:45 +0700

    [nft] Do not run nft command if it does not present on the system

--- etc/nftables/run_once_after_nft_apply.sh
+++ etc/nftables/run_once_after_nft_apply.sh
@@ -3,5 +3,7 @@
 # main.nft hash:				{{ include "main.nft" | sha256sum }}
 # hardening.nft hash:		{{ include "hardening.nft" | sha256sum }}
 # workstation.nft hash: {{ include "workstation.nft" | sha256sum }}
-nft -f ./main.nft
-rc-service nftables save
+if $(which nft >/dev/null 2>&1); then
+  nft -f ./main.nft
+  rc-service nftables save
+fi
